ab5f06f7e667b6ce4335cdd8da04332ac013da30
Fvelizb <155122487+Fvelizb@users.noreply.github.com>
2024-03-03 17:30:48 +0000

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,42 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const valores = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-  ];
-  const pintas = ["♦", "♥", "♠", "♣"];
+document.addEventListener('DOMContentLoaded', function() {
+  const valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  const pintas = ['♦', '♥', '♠', '♣'];
   const pintaColor = {
-    "♦": "red",
-    "♥": "red",
-    "♠": "black",
-    "♣": "black",
+    '♦': 'red',
+    '♥': 'red',
+    '♠': 'black',
+    '♣': 'black'
   };
 
   function generarCarta() {
     const valor = valores[Math.floor(Math.random() * valores.length)];
     const pinta = pintas[Math.floor(Math.random() * pintas.length)];
     const color = pintaColor[pinta];
-
-    document.querySelector(".cardValue").textContent = valor;
-    document.querySelectorAll(".icono").forEach((icon) => {
+    
+    document.querySelector('.cardValue').textContent = valor;
+    document.querySelectorAll('.icono').forEach(icon => {
       icon.textContent = pinta;
       icon.style.color = color;
     });
   }
 
-  document
-    .getElementById("generateButton")
-    .addEventListener("click", generarCarta);
-
+  document.getElementById('generateButton').addEventListener('click', generarCarta);
+  
   generarCarta();
 });
