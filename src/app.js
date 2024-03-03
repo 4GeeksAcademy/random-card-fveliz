bb6398b0192811652d62399083d1755374bc4864
Fvelizb <155122487+Fvelizb@users.noreply.github.com>
2024-03-03 17:25:53 +0000

    carta aleatoria

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.addEventListener("DOMContentLoaded", function() {
+  const valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  const pintas = ["♦", "♥", "♠", "♣"];
+  const pintaColor = {
+    "♦": "red",
+    "♥": "red",
+    "♠": "black",
+    "♣": "black",
+  };
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  function generarCarta() {
+    const valor = valores[Math.floor(Math.random() * valores.length)];
+    const pinta = pintas[Math.floor(Math.random() * pintas.length)];
+    const color = pintaColor[pinta];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    document.querySelector(".cardValue").textContent = valor;
+    document.querySelectorAll(".icono").forEach((icon) => {
+      icon.textContent = pinta;
+      icon.style.color = color;
+    });
+  }
+
+  document
+    .getElementById("generateButton")
+    .addEventListener("click", generarCarta);
+
+  generarCarta();
+});
